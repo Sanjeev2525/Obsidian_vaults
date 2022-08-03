5270c68fd6e687e208e689280587188a37d193cc
Sanjeev <sanjeevganesh25@gmail.com>
2022-08-03 17:14:47 +0530

    AM sync.sh

---
+++ sync.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+gstatus=`git status --porcelain`
+
+if [ ${#gstatus} -ne 0 ]
+then
+
+    git add --all
+    git commit -m "$gstatus"
+    sleep 10 
+    git pull
+    git push
+
+fi
